2d11cd3495378feb28fe1d9aaae3a2007a751b9a
ginedou0315 <ginelle.doubek@gmail.com>
2025-06-16 12:02:51 +0200

    added extensions for game build for Ionic

---
+++ android/app/src/main/java/gnl/gdoubek/puzzlepegcross/MainActivity.java
@@ -0,0 +1,5 @@
+package gnl.gdoubek.puzzlepegcross;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'gnl.gdoubek.puzzlepegcross',
+  appName: 'Puzzle_Peg_Cross',
+  webDir: 'build'
+};
+
+export default config;
